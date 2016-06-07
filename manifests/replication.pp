9cd89dedebfe499ca8103315aaa187a26505fa27
LaurentCrisci <laurent.crisci@itv.com>
2016-06-07 22:06:12 +0100

    Added source_database logic inside function
    Signed-off-by: LaurentCrisci <laurent.crisci@itv.com>

--- manifests/replication.pp
+++ manifests/replication.pp
@@ -3,18 +3,18 @@ define couchdb::replication (
   $password                = undef,
   $source_server           = '127.0.0.1',
   $target_server           = undef,
+  $source_database         = undef,
   $target_database         = undef,
   $continuous_replication  = true,
   $ensure                  = 'present',
 ) {
 
-  $source_database = $name
-
-  couchdb_replicate {"$source_database":
+  couchdb_replicate {"$name":
     username        => $username,
     password        => $password,
     source_server   => $source_server,
     target_server   => $target_server,
+    source_database => $source_database,
     target_database => $target_database,
     continuous      => $continuous_replication,
     ensure          => $ensure,
